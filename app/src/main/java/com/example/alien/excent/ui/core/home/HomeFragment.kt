4f7d69508c6bf46b10e538b14a3b75448c0434c9
Alejandro Ochoa <alejandro.ochoa@metova.com>
2018-10-30 18:02:12 -0600

    Create menu for home

--- app/src/main/java/com/example/alien/excent/ui/core/home/HomeFragment.kt
+++ app/src/main/java/com/example/alien/excent/ui/core/home/HomeFragment.kt
@@ -1,6 +1,8 @@
 package com.example.alien.excent.ui.core.home
 
 
+import android.os.Bundle
+import android.view.View
 import com.example.alien.excent.R
 import com.example.alien.excent.module.ApplicationComponentHolder
 import com.example.alien.excent.ui.base.ViewModelFragment
@@ -9,6 +11,12 @@ import com.example.alien.excent.ui.util.SnackbarUtil
 import com.metova.slim.annotation.Callback
 import com.metova.slim.annotation.Layout
 import javax.inject.Inject
+import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.Toolbar
+import android.view.Menu
+import android.view.MenuInflater
+import butterknife.BindView
+
 
 @Layout(R.layout.fragment_home)
 class HomeFragment : ViewModelFragment<HomeViewModel>() {
@@ -16,11 +24,30 @@ class HomeFragment : ViewModelFragment<HomeViewModel>() {
     @Inject
     lateinit var snackbarUtil: SnackbarUtil
 
+    @BindView(R.id.toolbar_home) lateinit var toolbarHome: Toolbar
+
     @Callback
     lateinit var  navigation: Navigation
 
     override fun viewModelClass() = HomeViewModel::class.java
 
     override fun inject() = ApplicationComponentHolder.INSTANCE.getComponent().inject(this)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        configureToolbar()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?, inflater: MenuInflater?) {
+        inflater!!.inflate(R.menu.menu_home, menu)
+        return super.onCreateOptionsMenu(menu, inflater)
+    }
+
+    fun configureToolbar() {
+        (activity as AppCompatActivity).setSupportActionBar(toolbarHome)
+        setHasOptionsMenu(true)
+        (activity as AppCompatActivity).supportActionBar!!.setDisplayShowTitleEnabled(false)
+    }
 
 }
