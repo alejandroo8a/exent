a10895387dbac36a8868ab21915a6e91d338ed12
Alejandro Ochoa <alejandro.ochoa@metova.com>
2019-02-22 15:56:38 -0600

    Add titles to some screens

--- app/src/main/java/com/example/alien/excent/ui/settings/SettingsHeader.kt
+++ app/src/main/java/com/example/alien/excent/ui/settings/SettingsHeader.kt
@@ -1,17 +1,24 @@
 package com.example.alien.excent.ui.settings
 
 import android.content.Context
+import android.graphics.drawable.Drawable
 import android.support.constraint.ConstraintLayout
+import android.support.v4.content.ContextCompat
 import android.util.AttributeSet
+import android.widget.ImageView
 import android.widget.TextView
 import butterknife.BindView
 import butterknife.ButterKnife
+import com.bumptech.glide.Glide
 import com.example.alien.excent.R
 
 class SettingsHeader : ConstraintLayout {
 
     @BindView(R.id.txt_title) lateinit var txtTitle : TextView
+    @BindView(R.id.im_settings) lateinit var imSettings : ImageView
     private lateinit var title : String
+    private var drawable : Drawable? = null
+    private lateinit var innerContext : Context
 
     constructor(context: Context?) : super(context)
 
@@ -28,10 +35,13 @@ class SettingsHeader : ConstraintLayout {
     }
 
     private fun init(attrs: AttributeSet) {
-
+        this.innerContext = context
         val styledAttributes = context.obtainStyledAttributes(attrs, R.styleable.SettingsHeader)
         try {
             title = styledAttributes.getString(R.styleable.SettingsHeader_title)
+            if(styledAttributes.getDrawable(R.styleable.SettingsHeader_image) != null) {
+                drawable = styledAttributes.getDrawable(R.styleable.SettingsHeader_image)
+            }
         } finally {
             styledAttributes.recycle()
         }
@@ -43,5 +53,16 @@ class SettingsHeader : ConstraintLayout {
         ButterKnife.bind(this)
 
         txtTitle.text = this.title
+        if(this.drawable != null) {
+            Glide.with(this.innerContext).load(this.drawable).into(imSettings)
+        }
+    }
+
+    fun setTitle(titleOut: String) {
+        txtTitle.text = titleOut
+    }
+
+    fun setImage(imageOut: Int) {
+        Glide.with(this.innerContext).load(ContextCompat.getDrawable(this.context, imageOut)).into(imSettings)
     }
 }
